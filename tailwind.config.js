52fdfc38e7ccfa0810d83301acfdc961deaceee4
S-a-b-b-i-R <sabbirssmc90@gmail.com>
2023-07-23 21:40:14 +0600

    fixed some issues and included a rating badge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@
 module.exports = {
     content: ["./index.html"],
     theme: {
-        extend: {
-            backgroundImage: {
-                "btn-gradient":
-                    "linear-gradient(270deg, #F00 0%, #FF8938 100%)",
-            },
-        },
+        extend: {},
     },
     plugins: [],
 };
